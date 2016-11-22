66f6e9e0f70c48e83c976232c33e3da2f202e22c
stuevans62 <stuart.evans@rbwm.gov.uk>
2016-11-22 08:32:46 +0000

    Html page to test the button.js src file
    Html page to test the button.js src file

---
+++ Button.js
@@ -0,0 +1,11 @@
+
+document.write('<a id="referback" href="https://www.rbwm.gov.uk">')
+document.write('   <input type="button" id="backbutton" value="RETURN 33"/>')
+document.write('</a>')
+
+window.onload = function() {
+	var url = document.getElementById("xforms_referring_page").value;	
+	var link = document.getElementById("referback");
+	link.setAttribute("href", url);	
+	document.getElementById("backbutton").value = "Return to Sender";
+}
